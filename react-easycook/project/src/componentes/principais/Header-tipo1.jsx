6c1b99e523503cd0339b44f21bd9be0f57cb2201
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-09-08 12:32:36 -0300

    iniciando react

---
+++ react-easycook/project/src/componentes/principais/Header-tipo1.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Header(){
+   return (
+   <div class="header-tipo1">
+        <a href="">
+            <img src="imagens/icone-seta-branca.png" class="imagem-seta"/>
+        </a>
+        <h2> Novo Ingrediente </h2>
+    </div>
+   )
+}
--- react-easycook/project/src/index.js
+++ react-easycook/project/src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import HeaderTipo1 from './componentes/principais/Header-tipo1'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div>
+    <HeaderTipo1></HeaderTipo1>
+  </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
