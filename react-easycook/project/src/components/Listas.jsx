909fd94ba763d19b19a4d0ae9bf999bc85e885d3
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-09-20 14:26:16 -0300

    surtando

--- react-easycook/project/src/components/Listas.jsx
+++ react-easycook/project/src/components/Listas.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import '../styles/listas.css'
 import Ingrediente from './Ingrediente'
+import { Link } from "react-router-dom"
 
 export function Lista({novo, nomeObjetos}) {
-    
+     
     return (
         <div id="listas">
             <button className="novo-objeto">{nomeObjetos}</button>
