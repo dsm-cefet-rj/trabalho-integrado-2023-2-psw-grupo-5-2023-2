3c1fa5dba3539a5b60dd14edefda3da59f357f26
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-09-21 02:17:12 -0300

    AjeitandoNovalistadecompras
    adicionei a parte escolher a  quantidade dos ingredientes

--- react-easycook/project/src/components/ContainerBodyLC.jsx
+++ react-easycook/project/src/components/ContainerBodyLC.jsx
@@ -22,6 +22,20 @@ export default function ContainerBodyLC(){
                     </div>
                 </div>
                 <div className='area3'>
+                <section style={{
+                backgroundColor: 'white',
+                borderRadius: '10px',
+                padding: '15px',
+                width: '190px'
+                }}>
+                <div style={{color: 'gray'}}>
+                <label for="">Ingrediente  <input  style={{width: '60px'}} class="qtd" type="number" name="Qtd" id="Qtd" placeholder="Qtd"/> Un</label><br />
+                <label for="">Ingrediente  <input style={{width: '60px'}}class="qtd" type="number" name="Qtd" id="Qtd" placeholder="Qtd"/> Kg</label><br />
+            <label>Ingrediente  <input style={{width: '60px'}}class="qtd" type="number" name="Qtd" id="Qtd" placeholder="Qtd"/> mL</label>
+            </div>
+                </section>
+                
+              
                     <div className='area-descricao'>
                         <label> Descrição </label>
                         <br></br>
