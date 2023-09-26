d148ae8aca833420ebfca030dd9b0adb91e23c65
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-09-25 22:39:17 -0300

    RemoverReceita

--- react-easycook/project/src/components/Receita.jsx
+++ react-easycook/project/src/components/Receita.jsx
@@ -7,6 +7,31 @@ export default function Receita({nome, categoriaPrincipal, id, descricao, ingred
     
     var [popup, setPopup] = useState(true);
     const tmp = ">";
+
+    const removeReceita = async () => {
+        const apiUrl = `http://localhost:3002/receitas/${id}`;
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        try {
+            const response = await fetch(apiUrl, requestOptions);
+            if (response.ok) {
+                console.log(`Receita com ID ${id} removida com sucesso.`);
+                // Adicione aqui qualquer ação que você deseja executar após a remoção
+            } else {
+                console.error(`Erro ao remover a receita com ID ${id}.`);
+            }
+        } catch (error) {
+            console.error(`Erro na solicitação DELETE: ${error}`);
+        }
+    };
+    const removeClick = () =>{
+        removeReceita(id)
+    }
     return(
         <div id="receita" className="componente">
             <label id="nome">{nome}</label>
@@ -19,7 +44,7 @@ export default function Receita({nome, categoriaPrincipal, id, descricao, ingred
             </div>
             <div id="popup-detalhes-excluir" hidden={popup}>
                   <Link to={`/detalhes-receita/${id}`}><h3 className="cinza">Detalhes</h3></Link>
-                  <h3 className="vermelho" onClick={e => setPopup(!popup)}>Remover</h3>
+                  <h3 className="vermelho" onClick={removeClick}>Remover</h3>
             </div>
         </div>
     )
