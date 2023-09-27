15d38c2776cf7701ac7c0b4dd6ccafafdb233f4d
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-09-27 13:27:55 -0300

    create button favoritar receita

--- react-easycook/project/src/components/Receita.jsx
+++ react-easycook/project/src/components/Receita.jsx
@@ -45,6 +45,7 @@ export default function Receita({nome, categoriaPrincipal, id, descricao, ingred
             <div id="popup-detalhes-excluir" hidden={popup}>
                   <Link to={`/detalhes-receita/${id}`}><h3 className="cinza">Detalhes</h3></Link>
                   <h3 className="vermelho" onClick={HandleRemoveReceitaClick}>Remover</h3>
+                  <h3 className="favoritar" onClick=''>Favoritar</h3>
             </div>
         </div>
     )
