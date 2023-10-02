8c2797058a4add80105f5521566c13ab9533610a
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-10-02 15:01:37 -0300

    icon 3 pontos

--- react-easycook/project/src/components/Cabecalho.jsx
+++ react-easycook/project/src/components/Cabecalho.jsx
@@ -50,9 +50,11 @@ export default function Cabecalho({titulo, fundo = 'claro', outros, filtro, home
               
           </div>
           <div className="rightSide">
-                        
-              <img src={tresPontos} hidden={!outros} alt="icone-tres-pontos" className="icone-tres-pontos"  onClick={popupConsultaIngrediente}/>
-              <svg xmlns="http://www.w3.org/2000/svg" hidden={favorito} width="50" height="50" fill="#ff2b2b" className="bi bi-bookmark-star-fill icone-favoritar" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" hidden={!outros} width="50" height="50" fill="#ff2b2b" class="bi bi-three-dots-vertical icone-tres-pontos" viewBox="0 0 16 16" onClick={popupConsultaIngrediente}>
+                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+              </svg>         
+
+              <svg xmlns="http://www.w3.org/2000/svg" hidden={!favorito} width="50" height="50" fill="#ff2b2b" className="bi bi-bookmark-star-fill icone-favoritar" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z"/>
               </svg>
           
