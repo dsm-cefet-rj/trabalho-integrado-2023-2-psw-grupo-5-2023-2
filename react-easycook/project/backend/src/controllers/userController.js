a882466c18bb641fd7076635b14464a7aae47ecb
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-10-06 14:48:15 -0300

    create e read usuario backend

--- react-easycook/project/backend/src/controllers/userController.js
+++ react-easycook/project/backend/src/controllers/userController.js
@@ -1,18 +1,29 @@
-import Usuario from '../../models/userSchema.js'
+import userSchema from '../../models/userSchema.js'
 
-
-    
-    function read(request, response){
-        return response.json({
-            success: true
-        })
+    async function read(request, response){
+        const userList = await userSchema.find();
+        return response.json(userList)
     }
 
-    function create(request, response){
+    async function create(request, response){
         const {id, userNome, userEmail, userCpf, userPassword, userDataNasc, createdAt} = request.body
 
-        console.log(id)
-        console.log(userNome)
+        if(!userNome || !id){
+            //erro
+            return response.status(400).json({
+                erro: 'necessario preencher nome e id'})
+        }
+
+        const userCreated = await userSchema.create({
+            id,
+            userNome,
+            userEmail,
+            userCpf,
+            userPassword,
+            userDataNasc,
+            createdAt
+        })
+        return response.json(userCreated)
     }
  export default {create, read}
 
