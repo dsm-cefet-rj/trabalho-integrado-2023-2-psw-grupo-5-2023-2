82ed59d8d15022fc55e3028b4b752d08259d48b1
Ênnya Gomes <ennyagomesoc@gmail.com>
2023-10-07 23:17:27 -0300

    TooltipCabeçalho

--- react-easycook/project/src/components/Cabecalho.jsx
+++ react-easycook/project/src/components/Cabecalho.jsx
@@ -63,7 +63,7 @@ export default function Cabecalho({titulo, fundo = 'claro', outros, filtro, home
               </svg>
               
               
-              <Link to="/" hidden={!home}>
+              <Link to="/" hidden={!home} class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Página Inicial">
                 <svg xmlns="http://www.w3.org/2000/svg" hidden={!home} width="50" height="50" fill="#ff2b2b" className="icone-home bi bi-house-fill" viewBox="0 0 16 16">
                   <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
                   <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
