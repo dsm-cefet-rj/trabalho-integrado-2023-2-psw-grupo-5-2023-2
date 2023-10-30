9db0b6a0995f4fe47cb96104cb7775979acf909d
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-10-30 13:54:14 -0300

    create estoqueSchema associado a cada usuario por id

---
+++ react-easycook/backend/src/models/estoqueSchema.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import normalize from 'normalize-mongoose'
+const Schema = mongoose.Schema
+
+const estoqueSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    },
+    ingredientes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Ingrediente'
+    }]
+});
+
+estoqueSchema.plugin(normalize);
+
+export default mongoose.model('Estoque', estoqueSchema);
