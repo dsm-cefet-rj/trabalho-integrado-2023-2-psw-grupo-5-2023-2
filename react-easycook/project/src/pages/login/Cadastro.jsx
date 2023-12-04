8fdc0c8216c9c3304618fef53e31333709696f74
RafoDias <74609193+RafoDias@users.noreply.github.com>
2023-12-03 23:20:38 -0300

    cadastro e login com validacoes frontend

--- react-easycook/project/src/pages/login/Cadastro.jsx
+++ react-easycook/project/src/pages/login/Cadastro.jsx
@@ -1,48 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/easycook1-nobg.png";
 import "../../styles/Cadastro.css";
-import { FetchScript } from "../../scripts/ApiBackend";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import { isEmail } from "validator";
 
-import AuthService from "../services/auth.service";
+import AuthService from "../../auth/auth.service";
 
-const required = value => {
+const required = (value) => {
   if (!value) {
     return (
       <div className="alert alert-danger" role="alert">
-        This field is required!
+        É necessário preencher esse campo.
       </div>
     );
   }
 };
-const email = value => {
+const vemail = (value) => {
   if (!isEmail(value)) {
     return (
       <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
-};
-const vusername = value => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
+        Esse e-mail não é válido.
       </div>
     );
   }
 };
 
-const vpassword = value => {
+const vpassword = (value) => {
   if (value.length < 6 || value.length > 40) {
     return (
       <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
+        A senha precisa ter entre 6 e 40 caracteres.
       </div>
     );
   }
@@ -52,11 +42,8 @@ export default class Cadastro extends Component {
   constructor(props) {
     super(props);
     this.handleRegister = this.handleRegister.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
-    this.onChangeCpf = this.onChangeCpf.bind(this);
-    this.onChangeDataNasc = this.onChangeDataNasc.bind(this);
     this.onChangeId = this.onChangeId.bind(this);
 
     this.state = {
@@ -64,46 +51,32 @@ export default class Cadastro extends Component {
       username: "",
       email: "",
       password: "",
-      cpf: "",
-      dataNasc: "",
       successful: false,
-      message: ""
+      message: "",
     };
   }
 
   onChangeUsername(e) {
     this.setState({
-      username: e.target.value
+      username: e.target.value,
     });
   }
 
   onChangeEmail(e) {
     this.setState({
-      email: e.target.value
+      email: e.target.value,
     });
   }
 
   onChangePassword(e) {
     this.setState({
-      password: e.target.value
-    });
-  }
-
-  onChangeCpf(e) {
-    this.setState({
-      cpf: e.target.value
-    });
-  }
-
-  onChangeDataNasc(e) {
-    this.setState({
-      dataNasc: e.target.value
+      password: e.target.value,
     });
   }
 
   onChangeId(e) {
     this.setState({
-      id: e.target.value
+      id: e.target.value,
     });
   }
 
@@ -112,7 +85,7 @@ export default class Cadastro extends Component {
 
     this.setState({
       message: "",
-      successful: false
+      successful: false,
     });
 
     this.form.validateAll();
@@ -125,13 +98,13 @@ export default class Cadastro extends Component {
         this.state.dataNasc,
         this.state.id
       ).then(
-        response => {
+        (response) => {
           this.setState({
             message: response.data.message,
-            successful: true
+            successful: true,
           });
         },
-        error => {
+        (error) => {
           const resMessage =
             (error.response &&
               error.response.data &&
@@ -141,7 +114,7 @@ export default class Cadastro extends Component {
 
           this.setState({
             successful: false,
-            message: resMessage
+            message: resMessage,
           });
         }
       );
@@ -158,26 +131,11 @@ export default class Cadastro extends Component {
         />
         <Form
           onSubmit={this.handleRegister}
-          ref={c => {
+          ref={(c) => {
             this.form = c;
           }}
         >
-        {!this.state.successful && ( )}
-          <div className="row">
-            <div className="col-8 mx-auto">
-              <label htmlFor="nome">Nome</label>
-              <Input
-                id="nome"
-                placeholder="Nome Completo"
-                type="text"
-                name="userEmail"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                validations={[required, vusername]}
-                className="form-control bg-branco"
-              />
-            </div>
-          </div>
+          {!this.state.successful}
 
           <div className="row">
             <div className="col-8 mx-auto">
@@ -189,39 +147,7 @@ export default class Cadastro extends Component {
                 name="userEmail"
                 value={this.state.email}
                 onChange={this.onChangeEmail}
-                validations={[required, email]}
-                className="form-control bg-branco"
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-8 mx-auto">
-              <label htmlFor="userCpf">CPF</label>
-              <Input
-                id="userCpf"
-                placeholder="Seu Cadastro de Pessoa Física"
-                type="number"
-                name="userEmail"
-                value={this.state.cpf, this.state.id}
-                onChange={this.onChangeCpf, this.onChangeId}
-                validations={[required]}
-                className="form-control bg-branco"
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-8 mx-auto">
-              <label htmlFor="userDataNasc">Data de Nascimento</label>
-              <Input
-                id="userDataNasc"
-                placeholder="Sua data de nascimento"
-                type="date"
-                name="userDataNasc"
-                value={this.state.dataNasc}
-                onChange={this.onChangeDataNasc}
-                validations={[required]}
+                validations={[required, vemail]}
                 className="form-control bg-branco"
               />
             </div>
@@ -243,210 +169,36 @@ export default class Cadastro extends Component {
             </div>
           </div>
           <div className="row justify-content-center mt-3">
-            <button
-              id="button-cadastro"
-              className="btn btn-primary col-3 mx-2"
-              
-            >
+            <button id="button-cadastro" className="btn btn-primary col-3 mx-2">
               Cadastrar-se
             </button>
             <Link to="/login" className="btn btn-primary col-3 text-white mx-2">
               Voltar
             </Link>
           </div>
-         
 
           {this.state.message && (
-              <div className="form-group">
-                <div
-                  className={
-                    this.state.successful
-                      ? "alert alert-success"
-                      : "alert alert-danger"
-                  }
-                  role="alert"
-                >
-                  {this.state.message}
-                </div>
+            <div className="form-group">
+              <div
+                className={
+                  this.state.successful
+                    ? "alert alert-success"
+                    : "alert alert-danger"
+                }
+                role="alert"
+              >
+                {this.state.message}
               </div>
-            )}
-            <CheckButton
-              style={{ display: "none" }}
-              ref={c => {
-                this.checkBtn = c;
-              }}
-            />
+            </div>
+          )}
+          <CheckButton
+            style={{ display: "none" }}
+            ref={(c) => {
+              this.checkBtn = c;
+            }}
+          />
         </Form>
       </div>
     );
   }
 }
-
-/*
-export default function Login() {
-  const navigate = useNavigate();
-
-  const [userNome, setUserNome] = useState("");
-  const [userEmail, setUserEmail] = useState("");
-  const [userCpf, setUserCpf] = useState("");
-  const [userDataNasc, setUserDataNasc] = useState(null);
-  const [userPassword, setUserPassword] = useState("");
-  const [id, setId] = useState(Number);
-
-
-  let emailValue = "";
-  let passwordValue = "";
-
-  const userData = {
-    id: id,
-    userNome: userNome,
-    userEmail: userEmail,
-    userCpf: userCpf,
-    userPassword: userPassword,
-    userDataNasc: userDataNasc,
-  };
-
-  useEffect(() => {
-    function enableSubmitButton() {
-      let btn = document.getElementById("button-cadastro");
-      btn.style.background = "#B22828";
-      if (userNome && userEmail && userCpf && userDataNasc && userPassword) {
-        btn.style.background = "#ff2635";
-      }
-    }
-    enableSubmitButton();
-  }, [userNome, userEmail, userCpf, userDataNasc, userPassword]);
-
-  return (
-    <div className="container">
-      <img
-        src={logo}
-        alt="Easy Cook logo"
-        className="mx-auto d-block blend-mode"
-      />
-
-      <div className="row">
-        <div className="col-8 mx-auto">
-          <label htmlFor="nome">Nome</label>
-          <input
-            id="nome"
-            placeholder="Nome Completo"
-            type="text"
-            name="userEmail"
-            onChange={(event) => setUserNome(event.target.value)}
-            className="form-control bg-branco"
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-8 mx-auto">
-          <label htmlFor="userEmail">E-mail</label>
-          <input
-            id="userEmail"
-            placeholder="Seu e-mail"
-            type="email"
-            name="userEmail"
-            onChange={(event) => setUserEmail(event.target.value)}
-            className="form-control bg-branco"
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-8 mx-auto">
-          <label htmlFor="userCpf">CPF</label>
-          <input
-            id="userCpf"
-            placeholder="Seu Cadastro de Pessoa Física"
-            type="number"
-            name="userEmail"
-            onChange={(event) => {
-              setUserCpf(event.target.value);
-              setId(Number(userCpf));
-            }}
-            className="form-control bg-branco"
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-8 mx-auto">
-          <label htmlFor="userDataNasc">Data de Nascimento</label>
-          <input
-            id="userDataNasc"
-            placeholder="Sua data de nascimento"
-            type="date"
-            name="userDataNasc"
-            onChange={(event) => setUserDataNasc(event.target.value)}
-            className="form-control bg-branco"
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-8 mx-auto">
-          <label htmlFor="userPassword">Senha</label>
-          <input
-            id="userPassword"
-            placeholder="Sua senha"
-            type="password"
-            name="userPassword"
-            onChange={(event) => setUserPassword(event.target.value)}
-            className="form-control bg-branco"
-          />
-        </div>
-      </div>
-      <div className="row justify-content-center mt-3">
-        <button
-          id="button-cadastro"
-          className="btn btn-primary col-3 mx-2"
-          onClick={submitCadastro}
-        >
-          Cadastrar-se
-        </button>
-        <Link to="/login" className="btn btn-primary col-3 text-white mx-2">
-          Voltar
-        </Link>
-      </div>
-      <div style={{ visibility: 'hidden' }} id="verifica-cadastro" className="row justify-content-center mt-3 col-2 mx-auto" >
-            <p>Usuário Cadastrado</p>
-      </div>
-    </div>
-  );
-
-
-
-  async function submitCadastro() {
-    try{
-      // Send data to the backend via POST
-      console.log("Deveria acontecer algo");
-      console.log(`userData: ${userData}`);
-      console.log(`JSON.stringify(userData): ${JSON.stringify(userData)}`);
-
-      let body = JSON.stringify({
-        id,
-        userNome,
-        userEmail,
-        userCpf,
-        userPassword,
-        userDataNasc,
-      });
-
-      const response = FetchScript.postData(FetchScript.RequestPaths.usuarios, body);
-
-      if (response.status === 200) {
-      console.log('Cadastro realizado com SUCESSO')
-      document.getElementById("verifica-cadastro").style.visibility = "visible"
-      }
-      if (response.status === 400) {
-        console.log('erro')
-        document.getElementById("verifica-cadastro").style.visibility = "hidden"
-        }
-    } catch(error){
-     console.error('erro ao realizar o cadastro', error)
-     
-    }
-  
-  }
-}*/
--- react-easycook/project/src/pages/login/Login.jsx
+++ react-easycook/project/src/pages/login/Login.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/easycook1-nobg.png";
 import "../../styles/old.login.css";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
+import AuthService from "../../auth/auth.service";
 
-import AuthService from "../services/auth.service";
-
-const required = value => {
+const required = (value) => {
   if (!value) {
     return (
       <div className="alert alert-danger" role="alert">
@@ -25,23 +25,23 @@ export default class Login extends Component {
     this.onChangeUseremail = this.onChangeUseremail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
 
-    this.state = { 
+    this.state = {
       useremail: "",
       password: "",
       loading: false,
-      message: ""
+      message: "",
     };
   }
 
   onChangeUseremail(e) {
     this.setState({
-      useremail: e.target.value
+      useremail: e.target.value,
     });
   }
 
   onChangePassword(e) {
     this.setState({
-      password: e.target.value
+      password: e.target.value,
     });
   }
 
@@ -50,7 +50,7 @@ export default class Login extends Component {
 
     this.setState({
       message: "",
-      loading: true
+      loading: true,
     });
 
     this.form.validateAll();
@@ -61,7 +61,7 @@ export default class Login extends Component {
           this.props.router.navigate("/");
           window.location.reload();
         },
-        error => {
+        (error) => {
           const resMessage =
             (error.response &&
               error.response.data &&
@@ -71,236 +71,108 @@ export default class Login extends Component {
 
           this.setState({
             loading: false,
-            message: resMessage
+            message: resMessage,
           });
         }
       );
     } else {
       this.setState({
-        loading: false
+        loading: false,
       });
     }
   }
 
   render() {
-    <div className="p-5 vh-100 mx-auto col-lg-6 col-12">
-      <img
-        src={logo}
-        alt="Easy Cook logo"
-        className="mx-auto d-block blend-mode"
-      />
-      <Form
-        onSubmit={this.handleLogin}
-        ref={c => {
-          this.form = c;
-        }}
-      >
-        <div className="form-outline mb-4">
-          <label id="login" class="form-label">
-            Login
-          </label>
-          <Input
-            id="userEmail"
-            placeholder="Seu e-mail"
-            type="email"
-            className="form-control bg-branco"
-            name="userEmail"
-            value={this.state.useremail}
-            onChange={this.onChangeUseremail}
-            validations={[required]}
-          />
-        </div>
-
-        <div className="form-outline mb-4">
-          <Input
-            id="userPassword"
-            placeholder="Sua senha"
-            type="password"
-            className="form-control bg-branco"
-            name="userPassword"
-            value={this.state.password}
-            onChange={this.onChangePassword}
-            validations={[required]}
-          />
-        </div>
-
-        <div class="row mb-4">
-          <div className="col text-center">
-            <Link to="/cadastre-se">
-              <a
-                className="link-aliceblue justify-content-end"
-                href="localhost:3000"
-                id="cadastre-se"
-              >
-                Cadastre-se
-              </a>
-            </Link>
+    return (
+      <div className="p-5 vh-100 mx-auto col-lg-6 col-12">
+        <img
+          src={logo}
+          alt="Easy Cook logo"
+          className="mx-auto d-block blend-mode"
+        />
+        <Form
+          onSubmit={this.handleLogin}
+          ref={(c) => {
+            this.form = c;
+          }}
+        >
+          <div className="form-outline mb-4">
+            <label id="login" class="form-label">
+              Login
+            </label>
+            <Input
+              id="userEmail"
+              placeholder="Seu e-mail"
+              type="email"
+              className="form-control bg-branco"
+              name="userEmail"
+              value={this.state.useremail}
+              onChange={this.onChangeUseremail}
+              validations={[required]}
+            />
           </div>
 
-          <div className="col text-center">
-            <Link to="/esqueci-minha-senha">
-              <a className="link-aliceblue" href="localhost:3000">
-                Esqueci minha senha
-              </a>
-            </Link>
+          <div className="form-outline mb-4">
+            <Input
+              id="userPassword"
+              placeholder="Sua senha"
+              type="password"
+              className="form-control bg-branco"
+              name="userPassword"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+              validations={[required]}
+            />
           </div>
-        </div>
 
-        <div className="row justify-content-center">
-          <button className="btn btn-primary col-6" disabled={this.state.loading}>
-            {this.state.loading && (
-              <span className="spinner-border spinner-border-sm"></span>
-            )}
-            <span>Login</span>
+          <div class="row mb-4">
+            <div className="col text-center">
+              <Link to="/cadastre-se">
+                <a
+                  className="link-aliceblue justify-content-end"
+                  href="localhost:3000"
+                  id="cadastre-se"
+                >
+                  Cadastre-se
+                </a>
+              </Link>
+            </div>
 
-          </button>
-        </div>
-        {this.state.message && (
-          <div className="form-group">
-            <div className="alert alert-danger" role="alert">
-              {this.state.message}
+            <div className="col text-center">
+              <Link to="/esqueci-minha-senha">
+                <a className="link-aliceblue" href="localhost:3000">
+                  Esqueci minha senha
+                </a>
+              </Link>
             </div>
           </div>
-        )}
-        <CheckButton
-          style={{ display: "none" }}
-          ref={c => {
-            this.checkBtn = c;
-          }}
-        />
-      </Form>
-    </div>
-  }
-} 
 
-/*
-export default function Login() {
- 
-  let navigate = useNavigate();
-
-  const [userEmail, setUserEmail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [logando, setLogando] = useState("");
-  const [users, setUsers] = useState([
-    {
-      id: "11111111111",
-      userNome: "usuarioTeste",
-      userEmail: "@.com",
-      userCpf: "11111111111",
-      userPassword: "asd",
-      userDataNasc: "2023-09-24",
-    },
-  ]);
-
-  let emailValue = "";
-  let passwordValue = "";
-
-  useEffect(() => {
-    getApiData();
-  }, []);
-
-  const getApiData = async () => {
-    const response = await fetch("/usuario")
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch(console.log);
-  };
-
-  return (
-    <div className="p-5 vh-100 mx-auto col-lg-6 col-12">
-      <img
-        src={logo}
-        alt="Easy Cook logo"
-        className="mx-auto d-block blend-mode"
-      />
-      <div className="form-outline mb-4">
-        <label id="login" class="form-label">
-          Login
-        </label>
-        <input
-          id="userEmail"
-          placeholder="Seu e-mail"
-          type="email"
-          className="form-control bg-branco"
-          name="userEmail"
-          onChange={(event) => setUserEmail(event.target.value)}
-        />
-      </div>
-
-      <div className="form-outline mb-4">
-        <input
-          id="userPassword"
-          placeholder="Sua senha"
-          type="password"
-          className="form-control bg-branco"
-          name="userPassword"
-          onChange={(event) => setUserPassword(event.target.value)}
-        />
-      </div>
-
-      <div class="row mb-4">
-        <div className="col text-center">
-          <Link to="/cadastre-se">
-            <a
-              className="link-aliceblue justify-content-end"
-              href="localhost:3000"
-              id="cadastre-se"
+          <div className="row justify-content-center">
+            <button
+              className="btn btn-primary col-6"
+              disabled={this.state.loading}
             >
-              Cadastre-se
-            </a>
-          </Link>
-        </div>
-
-        <div className="col text-center">
-          <Link to="/esqueci-minha-senha">
-            <a className="link-aliceblue" href="localhost:3000">
-              Esqueci minha senha
-            </a>
-          </Link>
-        </div>
+              {this.state.loading && (
+                <span className="spinner-border spinner-border-sm"></span>
+              )}
+              <span>Login</span>
+            </button>
+          </div>
+          {this.state.message && (
+            <div className="form-group">
+              <div className="alert alert-danger" role="alert">
+                {this.state.message}
+              </div>
+            </div>
+          )}
+          <CheckButton
+            style={{ display: "none" }}
+            ref={(c) => {
+              this.checkBtn = c;
+            }}
+          />
+        </Form>
       </div>
-
-      <div className="row justify-content-center">
-        <button className="btn btn-primary col-6" onClick={clickLogin}>
-          Login
-        </button>
-      </div>
-    </div>
-  );
-
-  function clickLogin() {
-    let temp = "";
-    if (userEmail === "" || userPassword === "") {
-      temp = temp.concat("Os campos não podem estar vazios.");
-    }
-    if (!userEmail.includes("@") || !userEmail.includes(".com")) {
-      temp = temp + "Formato inválido de e-mail.";
-    }
-    console.log(users);
-    let i = 0;
-    for (i; i <= users.length; i++) {
-      console.log(i);
-      if (i == users.length) {
-        i = -1;
-        break;
-      }
-      const e = users[i];
-      if (e.userEmail === userEmail) {
-        break;
-      }
-    }
-
-
-    if (i > -1 && userPassword === users[i].userPassword) {
-      setLogando("Autenticado!");
-      navigate("/");
-      return;
-    } else {
-      setLogando("Email ou login invalido.");
-      return;
-    }
-
-    setLogando(temp);
+    );
   }
 }
-*/
