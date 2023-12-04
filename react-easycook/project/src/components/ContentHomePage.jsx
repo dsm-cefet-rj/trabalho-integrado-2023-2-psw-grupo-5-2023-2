8dd2dbbb860873ee4b0ae4e83efff828ed28789e
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-12-04 14:26:01 -0300

    homepage done

--- react-easycook/project/src/components/ContentHomePage.jsx
+++ react-easycook/project/src/components/ContentHomePage.jsx
@@ -3,14 +3,15 @@ import '../styles/contentHP.css'
 import { Link } from "react-router-dom"
 
 export default function ContentHP({ingredientes}){
-    const ingredientesPouco = ingredientes.filter(ingrediente => ingrediente.qtd < 2);
+    const ingredientesPouco = ingredientes.filter(ingrediente => ingrediente.qtd < 3 && ingrediente.qtd != 0);
+    const ingredienteNada = ingredientes.filter(ingrediente => ingrediente.qtd == 0);
     return (
         <div className='card-group'>
             <div className="card text-center bg-branco rounded-3 mx-2">
                 <h1 className="card-header border-0 cor-titulo">Em falta!</h1>
                 <div className="card-body">
                     <ul>
-                       {ingredientesPouco.map((ingrediente) => (
+                       {ingredienteNada.map((ingrediente) => (
                         <li key={ingrediente._id}>{ingrediente.nome}</li>
                        ))}
                     </ul>
@@ -20,9 +21,13 @@ export default function ContentHP({ingredientes}){
                 </div>
             </div>
                 <div className="card text-center bg-branco rounded-3 mx-auto">
-                    <h1 className="card-header border-0  cor-titulo">Em falta!</h1>
+                    <h1 className="card-header border-0  cor-titulo">Acabando!</h1>
                     <div className="card-body">
-                        <p>Lista de ingredientes ou modo de preparo da receita selecionada nesse painel.</p>
+                        <ul>
+                            {ingredientesPouco.map((ingrediente) => (
+                            <li key={ingrediente._id}>{ingrediente.nome}</li>
+                        ))}
+                        </ul>
                     </div>
                      
                     <div className="card-footer bg-branco border-0">
