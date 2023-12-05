7cca55ca107baa199518aa55161f0ba91f8e4f24
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-12-04 21:58:36 -0300

    consertando editar-ingrediente parte1

--- react-easycook/project/src/pages/estoque/EditarIngrediente.jsx
+++ react-easycook/project/src/pages/estoque/EditarIngrediente.jsx
@@ -1,55 +1,78 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Cabecalho from "../../components/compartilhados/Cabecalho";
 import "../../styles/editarIngrediente.css";
 import img from "../../images/quadrado-vinho.jpg";
+import {
+  CategoriasDeIngredientes,
+  OpcoesDeMedidas,
+  OpcoesDeVariacao,
+} from "../../listas";
+import { useParams } from "react-router-dom";
+import { FetchScript } from "../../scripts/ApiBackend";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function EditarIngrediente() {
+  let navigate = useNavigate();
+  const { id } = useParams();
+
+  const [nome, setnome] = useState("");
+  const [categoriaPrincipal, setcategoriaPrincipal] = useState(null);
+  const [ingrediente, setingrediente] = useState([]);
+
+  useEffect(() => {
+  FetchScript.getDataById(FetchScript.RequestPaths.ingredientes, id)
+    .then((response) => setingrediente(response));
+  }, [id]);
+  
+  const handleAtualizarIngrediente = () => {
+    const requestBody = {
+      nome,
+      categoriaPrincipal,
+    
+    };
+    
+    FetchScript.postData(FetchScript.RequestPaths.ingredientes, id, requestBody);
+    setTimeout(() => {
+      navigate(`/estoque/${id}`);
+    }, 150);
+
+  }
+
+  
   return (
     <div className="EditarIngrediente">
-      <Cabecalho titulo="Detalhes (EDITANDO)" salvar cancelar></Cabecalho>
+      <Cabecalho titulo="Detalhes (EDITANDO)" ></Cabecalho>
       <div className="corpo">
         <img src={img} class="imagem-quadrado" />
         <input
           id="edit-nome-ingrediente"
           type="text"
-          placeholder="Nome do Ingrediente"
+          value={nome}
+          onChange={(e) => setnome(e.target.value)}
+          placeholder={ingrediente.nome}
         />
         <span> Categoria </span>
-        <select id="edit-categoria-ingrediente">
-          <option> Categoria </option>
-          <option> Categoria 2 </option>
-          <option> Categoria 3 </option>
+        <select id="edit-categoria-ingrediente" value={categoriaPrincipal} onChange={(e) => setcategoriaPrincipal(e.target.value)}>
+        {CategoriasDeIngredientes.map((cat) => (
+              <option value={cat}>{cat}</option>
+            ))}
         </select>
-        <span> Substitutos </span>
-        <table id="tabela-substitutos">
-          <tr>
-            <td>
-              <select class="edit-select-ingrediente">
-                <option> Ingrediente 1 </option>
-                <option> Ingrediente 2 </option>
-                <option> Ingrediente 3 </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <select class="edit-select-ingrediente">
-                <option> Ingrediente 5 </option>
-                <option> Ingrediente 2 </option>
-                <option> Ingrediente 3 </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <select class="edit-select-ingrediente">
-                <option> Ingrediente 10 </option>
-                <option> Ingrediente 2 </option>
-                <option> Ingrediente 3 </option>
-              </select>
-            </td>
-          </tr>
-        </table>
+        <Link
+            to={`/detalhes-ingrediente/${id}`}
+            
+            className="salvar"
+          >
+            <button onClick={handleAtualizarIngrediente}>Salvar</button>
+          </Link>
+          
+          <Link
+            to={`/detalhes-ingrediente/${id}`}
+            
+            className="cancelar"
+          >
+            <button>Cancelar</button>
+          </Link>
       </div>
     </div>
   );
