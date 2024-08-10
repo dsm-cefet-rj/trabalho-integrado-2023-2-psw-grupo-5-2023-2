eaac20f4f33239fb5e1290bd3b749a008f7ba835
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-08-10 00:28:49 -0300

    Update DetalhesIngredientes.jsx

--- react-easycook/project/src/pages/estoque/DetalhesIngredientes.jsx
+++ react-easycook/project/src/pages/estoque/DetalhesIngredientes.jsx
@@ -8,29 +8,37 @@ import { Link } from "react-router-dom";
 
 export default function DetalhesIngrediente() {
   const { id } = useParams();
-
-  
-
-  const [ingrediente, setingrediente] = useState([]);
+  const [ingrediente, setingrediente] = useState({});
 
   useEffect(() => {
-  FetchScript.getDataById(FetchScript.RequestPaths.ingredientes, id)
-    .then((response) => setingrediente(response));
-  }, []);
+    // Sua lógica para buscar os detalhes do ingrediente por ID
+    // Atualize o estado do ingrediente com os dados recebidos da API
+    // Exemplo hipotético:
+    fetch(`http://localhost:3001/ingrediente/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setingrediente(data); // Atualiza o estado com os dados recebidos
+      })
+      .catch((error) => console.error('Erro ao buscar os detalhes do ingrediente:', error));
+  }, [id]);
 
   return (
     <div className="DetalhesIngrediente">
       <Cabecalho titulo="Detalhes" fundo="claro" outros home />
       <div className="corpo">
         <img src={img} className="imagem-quadrado" alt="img-igrediente" />
-        <h1> {ingrediente.nome} </h1>
-        <span> Categoria </span>
-
-        <p> {ingrediente.categoriaPrincipal} </p>
-
-        <Link to={`/editar-ingrediente/${id}`}>
-            <button>Editar</button>
-          </Link>
+        {ingrediente.nome && ingrediente.categoriaPrincipal ? (
+          <>
+            <h1>{ingrediente.nome}</h1>
+            <span>Categoria</span>
+            <p>{ingrediente.categoriaPrincipal}</p>
+            <Link to={`/editar-ingrediente/${id}`}>
+              <button>Editar</button>
+            </Link>
+          </>
+        ) : (
+          <p>Carregando detalhes do ingrediente...</p>
+        )}
       </div>
     </div>
   );
